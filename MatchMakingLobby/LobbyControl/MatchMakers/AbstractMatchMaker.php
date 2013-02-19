dbd16e9df2d5a04735384a02f4b1c6b511dc8fa3
Philippe Melot <philippe@melot.eu>
2013-02-19 17:04:19 +0100

    add debug output

--- MatchMakingLobby/LobbyControl/MatchMakers/AbstractMatchMaker.php
+++ MatchMakingLobby/LobbyControl/MatchMakers/AbstractMatchMaker.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @copyright   Copyright (c) 2009-2013 NADEO (http://www.nadeo.com)
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
@@ -7,6 +6,7 @@
  * @author      $Author: $:
  * @date        $Date: $:
  */
+
 namespace ManiaLivePlugins\MatchMakingLobby\LobbyControl\MatchMakers;
 
 use ManiaLivePlugins\MatchMakingLobby\LobbyControl\Match;
@@ -15,31 +15,34 @@ use ManiaLivePlugins\MatchMakingLobby\LobbyControl\PlayerInfo;
 
 abstract class AbstractMatchMaker extends \ManiaLib\Utils\Singleton
 {
+
 	const WAITING_STEP = 60;
 	const DISTANCE_THRESHOLD = 300;
-	
+
 	/**
 	 * if override to true. Override method distributePlayers to put players in different teams
 	 * @var bool
 	 */
 	protected $isTeamMode = false;
+
 	/** @var Helpers\Graph */
 	protected $graph;
-	
 	public $playerPerMatch = 0;
-	
+
 	function run($matchSize)
 	{
 		$matches = array();
 		$this->buildGraph();
-		
+
 		$nodes = $this->graph->getNodes();
 		while($nodes && $cliques = $this->graph->findCliques(reset($nodes), $matchSize, self::DISTANCE_THRESHOLD))
 		{
-			usort($cliques, function($a, $b) {
-				$radiusDiff = $a->getRadius() - $b->getRadius();
-				return $radiusDiff < 0 ? -1 : ($radiusDiff > 0 ? 1 : 0);
-			});
+			usort($cliques,
+				function($a, $b)
+				{
+					$radiusDiff = $a->getRadius() - $b->getRadius();
+					return $radiusDiff < 0 ? -1 : ($radiusDiff > 0 ? 1 : 0);
+				});
 			$matches[] = reset($cliques)->getNodes();
 			$this->graph->deleteNodes(reset($cliques)->getNodes());
 			$nodes = $this->graph->getNodes();
@@ -50,24 +53,24 @@ abstract class AbstractMatchMaker extends \ManiaLib\Utils\Singleton
 				$match->players = $m;
 				return $match;
 			}, $matches);
-		
+
 		if($this->isTeamMode)
 		{
-			$matches = array_map(array($this,'distributePlayers'), $matches);
+			$matches = array_map(array($this, 'distributePlayers'), $matches);
 		}
 
 		return $matches;
 	}
-	
+
 	protected function buildGraph()
 	{
 		$this->graph = new Helpers\Graph();
-		
+
 		$followers = PlayerInfo::GetReady();
 		while($player = array_shift($followers))
 			$this->graph->addNode($player->login, $this->computeDistances($player, $followers));
 	}
-	
+
 	/**
 	 * @param PlayerInfo $player
 	 * @param PlayerInfo[] $followers
@@ -80,7 +83,7 @@ abstract class AbstractMatchMaker extends \ManiaLib\Utils\Singleton
 			$distances[$follower->login] = $this->distance($player, $follower);
 		return $distances;
 	}
-	
+
 	/**
 	 * @param PlayerInfo $p1
 	 * @param PlayerInfo $p2
@@ -88,20 +91,24 @@ abstract class AbstractMatchMaker extends \ManiaLib\Utils\Singleton
 	protected function distance($p1, $p2)
 	{
 		$distance = abs($p1->ladderPoints - $p2->ladderPoints);
-		
+
 		// Waiting time coefficient
 		$waitingTime = $p1->getWaitingTime() + $p2->getWaitingTime();
 		$distance *= exp(-log(2) * $waitingTime / self::WAITING_STEP);
-		\ManiaLive\Utilities\Logger::getLog('matchMakingDebug')->write(sprintf('distance %d, P1:%s|P2:%s',$distance,$p1->login, $p2->login));
-		
+		\ManiaLive\Utilities\Logger::getLog('matchMakingDebug')->write(sprintf('distance %d, P1:%s|P2:%s', $distance,
+				$p1->login, $p2->login));
+		\ManiaLive\Utilities\Console::printlnFormatted(sprintf('distance %d, P1:%s|P2:%s', $distance,
+				$p1->login, $p2->login));
+
+
 		return $distance;
 	}
-	
+
 	protected function distributePlayers(Match $m)
 	{
 		
 	}
-	
+
 	abstract function getPlayerScore($login);
 }
 
