5cbd83342c7ecd035cedc0a7cdd1ca3325d0f18e
Philippe Melot <philippe@melot.eu>
2013-03-25 14:26:23 +0100

    waiting time is no more reset when an another player leave the match

--- MatchMakingLobby/Match/Plugin.php
+++ MatchMakingLobby/Match/Plugin.php
@@ -602,7 +602,10 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 	{
 		if($this->intervals[$state])
 		{
-			$this->nextTick = new \DateTime($this->intervals[$state]);
+			if($this->state != static::PLAYER_LEFT && $this->state != $state)
+			{
+				$this->nextTick = new \DateTime($this->intervals[$state]);
+			}
 			$this->enableTickerEvent();
 		}
 		else $this->disableTickerEvent();
