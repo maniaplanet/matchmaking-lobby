04fbfc194c3eeaec11a1bc72a9f294cf1d889e6f
Philippe Melot <philippe@melot.eu>
2013-04-16 10:10:06 +0200

    use new features

--- MatchMakingLobby/Match/Plugin.php
+++ MatchMakingLobby/Match/Plugin.php
@@ -181,7 +181,7 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 		//setup the Lobby info window
 		$this->updateLobbyWindow();
 
-//		$this->connection->customizeQuitDialog(null, '#qjoin='.$this->lobby->backLink, false);
+		$this->connection->customizeQuitDialog(null, '#qjoin='.$this->lobby->backLink, false);
 	}
 
 	function onUnload()
@@ -191,7 +191,7 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 			$this->matchMakingService->updateMatchState($this->matchId, Services\Match::FINISHED);
 			$this->end();
 		}
-//		$this->connection->customizeQuitDialog();
+		$this->connection->customizeQuitDialog();
 		parent::onUnload();
 	}
 
@@ -469,7 +469,7 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 		\ManiaLive\Utilities\Logger::debug(sprintf('Preparing match for %s (%s)',$this->lobby->login, implode(',', array_keys($this->players))));
 		$this->changeState(self::WAITING);
 		$this->waitingTime = 0;
-//		$this->connection->setForcedTeams(true);
+		$this->connection->setForcedTeams(true);
 	}
 
 	protected function sleep()
@@ -652,7 +652,7 @@ class Plugin extends \ManiaLive\PluginHandler\Plugin
 			$this->scriptName,
 			$this->titleIdString
 		);
-//		$this->connection->setForcedTeams(false);
+		$this->connection->setForcedTeams(false);
 		$this->sleep();
 	}
 
