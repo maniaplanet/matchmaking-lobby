5af617c0ad09f6ff88840603186d5386786a21e1
Philippe Melot <philippe@melot.eu>
2013-04-29 15:44:20 +0200

    change messages to display cancel on a new line

--- MatchMakingLobby/GUI/Joust.php
+++ MatchMakingLobby/GUI/Joust.php
@@ -24,7 +24,7 @@ class Joust extends AbstractGUI
 		$key = array_search($player, $m->players);
 		$opponentObj = \ManiaLive\Data\Storage::getInstance()->getPlayerObject($m->players[($key + 1) % 2]);
 		$opponent = ($opponentObj ? $opponentObj->nickName : $m->players[($key + 1) % 2]);
-		return sprintf('$0F0Match against $<%s$> starts in $<$FFF%%1 $>, F6 to cancel...', $opponent);
+		return sprintf('$0F0Match against $<%s$> starts in $<$FFF%%1 $>'."\n".'F6 to cancel...', $opponent);
 	}
 
 	function getIllegalLeaveText()
