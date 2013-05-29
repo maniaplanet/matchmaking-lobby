ce85ff04498b0414f8392390a18d13c2cee5a2b0
ToRRent1812 <torrent1812@gmail.com>
2013-05-29 20:38:22 +0200

    Update Undersiege.php

--- MatchMakingLobby/MatchSettings/Undersiege.php
+++ MatchMakingLobby/MatchSettings/Undersiege.php
@@ -5,15 +5,18 @@ class Undersiege implements MatchSettings
 {
     public function getLobbyScriptSettings()
     {
-        $rules = array('S_UseLobby' => true);
+        $rules = array(
+    		'S_UseLobby' => true,
+			'S_LobbyTimePerMap' => 1800
+		);
         return $rules;
     }
 
     public function getMatchScriptSettings()
     {
         $rules = array('S_UseLobby' => true);
-        $rules['S_WarmUpDuration'] = 30;
-  	$rules['S_NbRoundMax'] = 5;
+        $rules['S_WarmUpDuration'] = 20;
+  	    $rules['S_NbRoundMax'] = 5;
         return $rules;
     }   
 }
