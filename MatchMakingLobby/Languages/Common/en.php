cab5b189ffeb08f2c393b50d8dca25d1f3338785
Philippe Melot <philippe@melot.eu>
2013-06-04 12:01:26 +0200

    creation of methods and unified English dictionary

---
+++ MatchMakingLobby/Languages/Common/en.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @version     $Revision: $:
+ * @author      $Author: $:
+ * @date        $Date: $:
+ */
+
+$lang['notReadyPlayer'] = '$o$F90Press F6 to play a match$z';
+$lang['readyPlayer'] = 'Searching for a match, press F6 to cancel';
+$lang['needReadyPlayers'] = 'Waiting for more ready players';
+$lang['playerBackLabelPrefix'] = 'Welcome back. ';
+$lang['noServerAvailable'] = 'No match server available at the moment, please wait';
+$lang['illegalLeave'] = "A player left\nDo not leave, searching for a substitute";
+$lang['launchMatch'] =  "\$0F0Match starts in \$<\$FFF%1 \$>...\nF6 to cancel";
+$lang['matchInProgress'] = "You have a match in progress. Prepare to be transferred";
+$lang['suspended'] = '$F00You are suspended for %d minutes';
+$lang['matchOver'] = 'Match over. You will be transferred back.';
+$lang['giveUp'] = 'A player gave up\nDo not leave, searching for a substitute';
+$lang['deciding'] = 'Waiting for all player to connect before starting match';
+$lang['tooManyAllies'] = '$F00You have too many allies, maximum is %d';
+$lang['expectingReadyPlayers'] = 'A match is expecting a substitute';
+$lang['substituteMoved'] = '%s$<%s$> joined his match as a substitute.';
+$lang['matchJoin'] = '%s%s join a match.';
+$lang['matchCancel'] = '%s$<%s$> cancelled match start.';
+$lang['playerSuspended'] = '%s$<%s$> is suspended.';
+$lang['matchAborted'] = '%sMatch aborted.';
+$lang['matchDeciding'] = '%sMatch is starting, you can call a vote to change the map.';
+$lang['matchStarting'] = '%sMatch is starting. Good luck and have fun!';
+$lang['playerBack'] = '%sPlayer is back, match continues.';
+$lang['backupConnected'] = '%sSubstitutes are connected, match continues.';
+$lang['transfer'] = 'You\'re being transfered. Please wait.';
+$lang['helpSwitch'] = 'Press F7 for help';
+$lang['helpText'] = "You are on a matchmaking waiting room server.
+		
+%1\$s Press F6 to show that you are ready.
+%1\$s Play a fun mode while you wait.
+%1\$s Your %2\$s game will start automatically.
+%1\$s Press F7 to close this help.";
+$lang['helpTip'] = 'Tip: team-up with your buddies';
+$lang['helpNote'] = 'Note: both players have to set the other as ally.';
+$lang['wait'] = 'Please wait...';
+$lang['waitingTime'] = 'Estimated waiting time: %s';
+$lang['playing'] = 'En jeu';
+$lang['ready'] = 'Ready';
+?>
