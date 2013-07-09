781208e4c1a9a477126f1213dfe353975dd0ddc7
Baptiste Lafontaine <baptiste33@gmail.com>
2013-07-09 15:22:50 +0200

    Adding GUI for elite practice

---
+++ MatchMakingLobby/GUI/ElitePractice.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @version     $Revision: $:
+ * @author      $Author: $:
+ * @date        $Date: $:
+ */
+namespace ManiaLivePlugins\MatchMakingLobby\GUI;
+
+use ManiaLive\Gui\Windows\Shortkey;
+
+class ElitePractice extends Joust
+{
+	
+}
+?>
